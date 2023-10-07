84c20a28a9706751932a527d535640d7850281af
Chernyshov Artem <artemka2015kek@gmail.com>
2023-10-07 16:44:26 +0300

    Scripts stuff

--- Planetarium/src/Users.jl
+++ Planetarium/src/Users.jl
@@ -1,7 +1,6 @@
 module Users
     using Planetarium
     using Planetarium.DB
-
     struct User
         id::Integer = -1
         name::String
@@ -9,6 +8,7 @@ module Users
         balance::Float64
         password::String
     end
+    
     function getUser(id::Integer)
         userInfo = DB.executeQuery("SELECT * FROM Users WHERE id = $id")
         return User(
@@ -19,18 +19,10 @@ module Users
             userInfo[1,"password"]
         )
     end
-    function getUser(name::String)
-        userInfo = DB.executeQuery("SELECT * FROM Users WHERE name = '$name'")
-        return User(
-            userInfo[1,"id"],
-            userInfo[1,"name"],
-            userInfo[1,"email"],
-            userInfo[1,"balance"],
-            userInfo[1,"password"]
-        )
+    
+    function createUser(user::User)
+        DB.executeQuery("INSERT INTO Users VALUES(NULL,'$(user.name)','$(user.email)',0,'$(user.password)')")
     end
 
-    function createUser(user::User)
-        DB.executeQuery("INSERT INTO Users VALUES(NULL,'"*user.name *"','"*user.email *"',0,'"*user.password *"')")
-    end
+    
 end
