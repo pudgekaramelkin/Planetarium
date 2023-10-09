d1222f0ca9d00c800a8d9d0d3b31d1eacfdbe73d
Chernyshov Artem <artemka2015kek@gmail.com>
2023-10-10 02:15:27 +0300

    Enums and data modeling basically, working with space objects. Did you know that there are stars that are called 'Wolf-Rayet stars' ? They are a group of rare superluminous stars that are rapidly losing their mass through intense stellar winds. How tragic. Anyway///

--- Planetarium/src/SpaceObjects.jl
+++ Planetarium/src/SpaceObjects.jl
@@ -1,9 +1,35 @@
-module PlanetModule
+module SpaceObjects
     using Planetarium
     using Planetarium.DB
-
-    struct Planet
-        name::String = ""
-        distanceFromTheStar::Float64 = ""
+    @enum SpaceObjectClass begin
+        planet = 1
+        satellite = 2
+        star = 3
+        galaxy = 4
+        asteroid = 5
+        comet = 6
+        black_hole = 7
+        neutron_star = 8
+        brown_dwarf = 9
+        red_dwarf = 10
+        white_dwarf = 11
+        main_sequence_star = 12
+        giant_star = 13
+        supergiant_star = 14
+        hypergiant_star = 15
+        wolf_rayet_star = 16
     end
+    struct SpaceObject
+        id::Integer
+        name::String
+        ownerId::Integer
+        distance_from_owner::Float64
+        radius::Float64
+        orbit_coordinates::Vector{Float32}
+        composition::Vector{Pair}
+        temperature::Float32
+        class::SpaceObjectClass 
+    end
+
+    
 end
